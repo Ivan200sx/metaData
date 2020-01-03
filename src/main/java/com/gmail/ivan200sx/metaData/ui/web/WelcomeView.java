6fed24e12a2dbf616d953852aea5456160089457
Paz Maksim <zesetup@ya.ru>
2020-01-03 08:42:48 +0200

    Suggestions

--- src/main/java/com/gmail/ivan200sx/metaData/ui/web/WelcomeView.java
+++ src/main/java/com/gmail/ivan200sx/metaData/ui/web/WelcomeView.java
@@ -1,7 +1,12 @@
 package com.gmail.ivan200sx.metaData.ui.web;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
 import com.gmail.ivan200sx.metaData.WebWetherSgd;
 import com.gmail.ivan200sx.metaData.WebWetherTts;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
@@ -24,6 +29,7 @@ public class WelcomeView extends VerticalLayout {
 
   private static final long serialVersionUID = 1L;
   public static final String VIEW_NAME = "record";
+  private List<Checkbox> checkBoxes = new ArrayList<>();
 
   public WelcomeView() {
     VerticalLayout todosList = new VerticalLayout();
@@ -31,13 +37,12 @@ public class WelcomeView extends VerticalLayout {
     TextField taskField = new TextField();
 
 
-
-
     Button addButton = new Button("Add");
     addButton.addClickShortcut(Key.ENTER);
     addButton.addClickListener(click -> {
       if (!taskField.isEmpty()) {
         Checkbox checkbox = new Checkbox(taskField.getValue());
+        checkBoxes.add(checkbox);
         idCounter++;
         checkbox.setId("checkbox-" + idCounter);
         todosList.add(checkbox);
@@ -65,11 +70,46 @@ public class WelcomeView extends VerticalLayout {
 
       //addButton.setText("Fired");
       todosList.remove(addButton);
-
+      
+      checkBoxes.forEach(checkbox -> {
+        if(!checkbox.getValue()) {
+          
+        }
+      });
+      
+      // Not recommended
+      for(int i=0; i<checkBoxes.size(); i++) {
+        if(!checkBoxes.get(i).getValue()) {
+          
+        }
+      }
+      
+      Iterator<Checkbox> it = checkBoxes.iterator();
+      while(it.hasNext()) {
+        Checkbox ch = it.next();
+        if(!ch.getValue()) {
+          
+        }
+      }
+      
       for (int i = 0; i < idCounter; i++) {
         add(new Label("CLICK PERFORMS"));
 
       }
+
+      for(Checkbox ch: checkBoxes) {
+        if(!ch.getValue()) {
+
+        }
+      }
+      
+      Stream<Component> com = getChildren();
+      
+      com.forEach(c -> {
+        if( c.getClass().equals(Checkbox.class)) {
+          
+        }
+      });
 
     });
 
