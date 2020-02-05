578a3193c9b5da74700a8dddeffe22db90497fed
Mechtijev <mechtijev@iae.lt>
2020-02-05 15:14:54 +0200

    works

--- src/main/java/com/gmail/ivan200sx/metaData/ui/web/MainLayout.java
+++ src/main/java/com/gmail/ivan200sx/metaData/ui/web/MainLayout.java
@@ -3,6 +3,7 @@ package com.gmail.ivan200sx.metaData.ui.web;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
